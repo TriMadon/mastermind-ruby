e9ae5eee0d7477ed5d65870c9ab7809478d2847a
TriMadon <mehammed6789@gmail.com>
2023-09-05 15:48:28 +0300

    Add class initializations to resolve #1

--- mastermind.rb
+++ mastermind.rb
@@ -5,10 +5,30 @@ require './human_player'
 
 class Mastermind
   def initialize
+    @comp_p = ComputerPlayer.new
+    @human_p = HumanPlayer.new
+    @secret_code = @computer_player.create_code
 
+    welcome_message
   end
 
   def start
+    puts "Let's start!"
+    main_loop
+  end
 
+  def main_loop
+    "To be implemented"
+  end
+
+  private:
+
+  def welcome_message
+    puts "Welcome to Mastermind!\n\n"
+    puts "Instructions:"
+    puts "- Guess the 4-color secret code."
+    puts "- Colors are represented by numbers (1-6)."
+    puts "- You have 12 attempts."
+    puts "- Feedback about the accuracy of your guess is provided after each guess.\n"
   end
 end
