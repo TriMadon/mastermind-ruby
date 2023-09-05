29133062fdb666b1f3facba64cdec1d9ecbf37d5
TriMadon <mehammed6789@gmail.com>
2023-09-05 16:55:04 +0300

    Add main game loop to resolve #2

--- human_player.rb
+++ human_player.rb
@@ -4,4 +4,9 @@ class HumanPlayer
   def initialize
 
   end
+
+  def make_guess
+    puts "\nEnter your guess (e.g., 1234):"
+    gets.chomp.gsub(/[[:space:]]/, '')
+  end
 end
--- mastermind.rb
+++ mastermind.rb
@@ -8,6 +8,8 @@ class Mastermind
     @comp_p = ComputerPlayer.new
     @human_p = HumanPlayer.new
     @secret_code = @comp_p.create_code
+    @remaining_turns = 12
+    @is_correct = false
 
     welcome_message
   end
@@ -18,7 +20,18 @@ class Mastermind
   end
 
   def main_loop
-    "To be implemented"
+    until @remaining_turns == 0
+      puts "\nAttempts left: #{@remaining_turns}"
+      print_previous_guesses
+      guess = make_guess
+      if guess == @secret_code
+        @is_correct = true
+        break
+      end
+      @remaining_turns -= 1
+    end
+
+    @is_correct ? win_message : loss_message
   end
 
   private
@@ -30,5 +43,21 @@ class Mastermind
     puts "- Colors are represented by numbers (1-6)."
     puts "- You have 12 attempts."
     puts "- Feedback about the accuracy of your guess is provided after each guess."
+  end
+
+  def print_previous_guesses
+    puts "\nPrevious guesses are printed here:"
+  end
+
+  def make_guess
+    @human_p.make_guess
+  end
+
+  def win_message
+    puts "\nYou have guessed the secret code correctly. Congratulations!"
+  end
+
+  def loss_message
+    puts "\nYou have failed to guess the secret code within 12 turns"
   end
 end
