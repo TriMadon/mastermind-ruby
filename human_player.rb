25a850e24fffd122ad5441b91cd517453fc7aad6
TriMadon <mehammed6789@gmail.com>
2023-09-06 09:26:14 +0300

    Implement input validation for guesses

--- human_player.rb
+++ human_player.rb
@@ -6,7 +6,13 @@ class HumanPlayer
   end
 
   def make_guess
-    puts "\nEnter your guess (e.g., 1234):"
-    gets.chomp.gsub(/[[:space:]]/, '')
+    loop do
+      print "\nEnter your guess (e.g., 1234): "
+      guess = gets.chomp.gsub(/\s+/, '')
+
+      return guess if guess.length == 4 && guess.match?(/^[1-6]+$/)
+
+      puts 'Invalid input. Please enter a 4-digit guess using numbers 1 to 6.'
+    end
   end
 end
